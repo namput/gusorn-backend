26a51dae3594a700961245c082a116b647a7d237
namput <47818793+namput@users.noreply.github.com>
2025-02-20 18:31:03 +0700

    update2

--- routes/tutorRoutes.js
+++ routes/tutorRoutes.js
@@ -1,15 +1,9 @@
 const express = require("express");
-const { getTutorProfile, updateTutorProfile, createTutorProfile, createProfile, uploadTutorPhoto } = require("../controllers/tutorController");
+const { createProfile } = require("../controllers/tutorController");
 const { authenticateUser } = require("../middlewares/authMiddleware");
 const upload = require("../middlewares/uploadMiddleware"); // ✅ ใช้ Middleware ที่ถูกต้อง
 
 const router = express.Router();
-
-// ✅ เส้นทาง API สำหรับสร้างโปรไฟล์ติวเตอร์
-router.post("/create", authenticateUser, createTutorProfile);
-router.get("/profile", authenticateUser, getTutorProfile);
-router.put("/profile/update", authenticateUser, updateTutorProfile);
-router.put("/upload-photo", authenticateUser, upload.single("photo"), uploadTutorPhoto);
 
 // ✅ เส้นทาง API รองรับอัปโหลดไฟล์ (รูปโปรไฟล์ & วิดีโอแนะนำตัว)
 router.post("/create-profile", authenticateUser, upload.fields([{ name: "profileImage" }, { name: "introVideo" }]), createProfile);
